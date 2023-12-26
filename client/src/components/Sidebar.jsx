401141706ebdafeb461ef825993580f73b78eb8e
chopper <89061191+Cchopksd@users.noreply.github.com>
2023-12-26 08:38:10 +0700

    Update hold button selected

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,25 +1,43 @@
 import { Link } from "react-router-dom";
-import './styles/Sidebar.css'
+import { useState } from "react";
+import "./styles/Sidebar.css";
 
 const Sidebar = () => {
+    const [selectedLink] = useState(location.pathname);
+
     return (
         <main className='profile-sec-ops'>
             <nav className='profile-option'>
                 <Link
                     to={"/account/edit-profile"}
-                    className='profile-each-option'
+                    className={`profile-each-option ${
+                        selectedLink === `/account/edit-profile`
+                            ? "selected"
+                            : ""
+                    }`}
                 >
                     <b>Edit profile</b>
                 </Link>
                 <hr className='profile-line' />
                 <Link
                     to={"/account/change-password"}
-                    className='profile-each-option'
+                    className={`profile-each-option ${
+                        selectedLink === `/account/change-password`
+                            ? "selected"
+                            : ""
+                    }`}
                 >
                     <b>Change password</b>
                 </Link>
                 <hr className='profile-line' />
-                <Link className='profile-each-option'>
+                <Link
+                    to={"/account/address"}
+                    className={`profile-each-option ${
+                        selectedLink === `/account/address`
+                        ? "selected"
+                        : ""
+                    }`}
+                >
                     <b>Address</b>
                 </Link>
                 <hr className='profile-line' />
