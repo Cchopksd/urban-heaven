d7ee4988f3a52be0a28586da270e5741054b9c57
chopper <89061191+Cchopksd@users.noreply.github.com>
2023-12-28 15:29:01 +0700

    Update connectDB.js

--- server/configs/connectDB.js
+++ server/configs/connectDB.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg')
 
 exports.databaseConfig = new Pool({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    database: process.env.DB_DATABASE || 'chat_app',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
-    // connectionString: process.env.DATABASE_URL,
+    // host: process.env.DB_HOST || 'localhost',
+    // port: process.env.DB_PORT || 5432,
+    // database: process.env.DB_DATABASE || 'chat_app',
+    // user: process.env.DB_USER || 'postgres',
+    // password: process.env.DB_PASSWORD || 'password',
+    connectionString: process.env.DATABASE_URL,
 })
