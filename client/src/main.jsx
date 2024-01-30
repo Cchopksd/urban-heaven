d9632b8c781e042da18411e6db8187c7dcab559f
chopper <89061191+Cchopksd@users.noreply.github.com>
2024-01-30 23:39:42 +0700

    use redux-toolkit instead contextAPI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './Routes.jsx';
 import './index.css';
-import { Provider } from './context/Provider.jsx';
+// import { Provider } from './context/Provider.jsx';
+import { store, persistor } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<Provider>
-		<I18nextProvider i18n={i18n}>
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>
-		</I18nextProvider>
+	<Provider store={store}>
+		<PersistGate
+			loading={null}
+			persistor={persistor}>
+			<I18nextProvider i18n={i18n}>
+				<React.StrictMode>
+					<App />
+				</React.StrictMode>
+			</I18nextProvider>
+		</PersistGate>
 	</Provider>,
 );
