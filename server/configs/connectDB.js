961aa1f57aaac2e9a319a21c2f80734ea880c8c7
chopper <89061191+Cchopksd@users.noreply.github.com>
2024-02-26 01:29:33 +0700

    Create connectDB.js

---
+++ server/configs/connectDB.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+// console.log(process.env.DATABASE_URL)
+
+exports.databaseConfig = new Pool({
+	host: process.env.POSTGRES_HOST || 'localhost',
+	port: process.env.POSTGRES_PORT || 5432,
+	database: process.env.POSTGRES_DATABASE || 'urban-heaven',
+	user: process.env.POSTGRES_USER || 'postgres',
+	password: process.env.POSTGRES_PASSWORD || 'password',
+	connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+});
