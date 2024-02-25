555307bce028eba7d2698b26f0074fd0df42cee7
chopper <89061191+Cchopksd@users.noreply.github.com>
2024-02-25 22:08:29 +0700

    Change prop types of children

--- client/src/Routes/IsLoggedIn.jsx
+++ client/src/Routes/IsLoggedIn.jsx
@@ -4,15 +4,18 @@ import PropTypes from 'prop-types';
 
 const IsLoggedIn = ({ children }) => {
 	const { status, isUserLoggedIn } = useSelector((state) => state.auth);
+
 	if (status === 'loading') return null;
 	else if (status === 'failed' || isUserLoggedIn === false) return children;
 	else if (isUserLoggedIn === true) {
 		return <Navigate to={'/'} />;
+	} else {
+		return null;
 	}
 };
 
 export default IsLoggedIn;
 
 IsLoggedIn.propTypes = {
-	children: PropTypes.params,
+	children: PropTypes.node,
 };
