6c875dadd9522bd1342347d62eb76f39a5d97a0e
chopper <89061191+Cchopksd@users.noreply.github.com>
2024-03-09 20:15:44 +0700

    image

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -2,6 +2,11 @@ const cloudinary = require('../configs/cloudinary');
 
 exports.sendImage = async (req, res) => {
 	try {
+		console.log(req.file);
+		if (!req.file) {
+			return res.status(400).json({ message: 'No file uploaded' });
+		}
+
 		const b64 = Buffer.from(req.file.buffer).toString('base64');
 		let imgURI = 'data:' + req.file.mimetype + ';base64,' + b64;
 
