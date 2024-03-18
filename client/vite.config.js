44e26a4531d4e0b821b54caab3b1750c5dc5a19e
chopper <89061191+Cchopksd@users.noreply.github.com>
2024-03-18 09:41:53 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite';
+import fs from 'fs';
 import react from '@vitejs/plugin-react-swc';
+import mkcert from 'vite-plugin-mkcert';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
+		// https: {
+		// 	key: fs.readFileSync('./ssl/private-key.key'),
+		// 	cert: fs.readFileSync('./ssl/urban-heaven_me.crt'),
+		// 	ca: fs.readFileSync('./ssl/urban-heaven_me.ca-bundle'),
+		// },
 		watch: {
 			usePolling: true,
 		},
-		host: true, // needed for the Docker Container port mapping to work
+		host: true,
 		strictPort: true,
 		port: 5173, // you can replace this port with any port
 	},
